d0b5c6e1981b59dfd4eb8d5e64dce4d018e16979
GabrielJ <yordivolcano@gmail.com>
2023-09-15 22:14:10 -0400

    Update at home

---
+++ database.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '9547',
+    port: 5432
+})
+
+pool.connect((err, client, release) => {
+    if(err) {
+        console.log('Error al conectarse a la BBDD')
+        return
+    }
+})
+
+
+module.exports = pool
--- routes/post.js
+++ routes/post.js
@@ -1,23 +1,34 @@
 const express = require('express'),
       router = express.Router()
 const fs = require('fs')
+const pool = require('../database')   
 
-router.get('/:post', (req, res, next) => {
+
+router.get('/:post', async (req, res, next) => {
     const post = req.params.post
 
-    fs.access(`views/posts/${post}`, (err) => {
-      if (err) {
-        next()
-      }
+    const { rows } = await pool.query('SELECT * FROM posts WHERE src = "?"', [post])
+    console.log(rows)
     
+    /*
+    if (!data) {
+
+      fs.access(`views/posts/${post}`, (err) => {
+        if (err) {n
+          next()
+        }
+      })
+
       res.render(`posts/${post}`, {
         title: 'JordiG - Blog - JavaScript - Productividad'
       })
-    })
-    
+    } else {
+        console.log('error')
+        return 
+    }
+    */
 
-
-
+ 
 })
 
 
