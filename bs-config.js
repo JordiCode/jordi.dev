4fa2fa024ce644e83150d605c2b3a70a1a335cab
GabrielJ <yordivolcano@gmail.com>
2024-05-05 23:10:15 -0400

    remove project previus

--- bs-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  proxy: "http://localhost:3000", 
-  files: ['locales/**/*', 'public/**/*', 'src/views/**/*', 'src/partials/**/*'], 
-  port: 4000
-}
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const browserSync = require('browser-sync').create();
-const PORT = 3000
-
-app.listen(PORT)
-
-
