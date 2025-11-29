5b9980eda77fb72cfd32a00cb0f7f14545d2c4d2
gabrielj <yordivolcano@gmail.com>
2025-11-29 00:19:36 -0400

    Observer API applied to Images, Reduction of images size, Contact Widget Added, Link Component Added

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -1,88 +1,83 @@
-import AstroIcon from "../components/icons/AstroIcon";
-import FigmaIcon from "../components/icons/FigmaIcon";
-import JavaScriptIcon from "../components/icons/JavaScriptIcon";
-import TailWindCssIcon from "../components/icons/TailWindCssIcon";
-
 const data = [
   {
     status: "building",
     linkPreview:
       "https://www.figma.com/design/FQ8MnQH6LaXT5WSTphUHjT/Archit?node-id=0-1&t=rcTd4OLT7H4PB3C9-1",
     linkCode: undefined,
-    srcImg: "project-archit.png",
+    srcImg: "projects/project-archit.png",
     title: "Archit (Productive App)",
     description: `This is a community app that allow the users to share challenges about any topic, know new people, track their time-work... it's an environment for people can reach their goals`,
-    tools: [FigmaIcon],
+    tools: ["figma"],
   },
   {
     status: "building",
     linkPreview:
       "https://www.figma.com/design/j6NhW3EjNLkiEPZ4Scqqmb/AppLanguages?node-id=0-1&t=rcTd4OLT7H4PB3C9-1",
     linkCode: undefined,
-    srcImg: "project-app-languages.png",
+    srcImg: "projects/project-app-languages.png",
     title: "ComLang (Language App)",
     description: `ComLang allow you to increase your vocabulary in any language by tracking the new word you found across, allow you to save your favorite material to review constantly, see the stadistic about your progress and the posibility the reading and listening content at the same time`,
-    tools: [FigmaIcon],
+    tools: ["figma"],
   },
   {
     status: "production",
     linkPreview: "https://www.premezcladosdehormigonfjp.com",
     linkCode: undefined,
-    srcImg: "project-fjp.jpg",
+    srcImg: "projects/project-fjp.jpg",
     title: "Premezclados de hormigon FJP",
     description: `Construction Company. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [JavaScriptIcon],
+    tools: ["javascript"],
   },
   {
     status: "production",
     linkPreview: "https://www.salem-taxi.com",
     linkCode: undefined,
-    srcImg: "project-salem-taxi.png",
+    srcImg: "projects/project-salem-taxi.png",
     title: "Salem Taxi LLC",
     description: `Taxi Services Company. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [AstroIcon, TailWindCssIcon],
+    tools: ["astro", "tailwindcss"],
   },
   {
     status: "production",
     linkPreview: "https://www.americabtaxisalem.com/",
     linkCode: undefined,
-    srcImg: "project-americab-taxi-salem.png",
+    srcImg: "projects/project-americab-taxi-salem.png",
     title: "Americab Taxi Salem",
     description: `Taxi Services Company. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [AstroIcon, TailWindCssIcon],
+    tools: ["astro", "tailwindcss"],
   },
   {
     status: "production",
     linkPreview: "https://www.lawrencecollisioncenterinc.com",
     linkCode: undefined,
-    srcImg: "project-lawrance-collision-center.png",
+    srcImg: "projects/project-lawrance-collision-center.png",
     title: "Lawrance Collision Center INC",
     description: `Services about Repair Vehicules. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [AstroIcon, TailWindCssIcon],
+    tools: ["astro", "tailwindcss"],
   },
   {
     status: "production",
     linkPreview: "https://www.rocknrollblunts.com",
     linkCode: undefined,
-    srcImg: "project-rock-n-roll.png",
+    srcImg: "projects/project-rock-n-roll.png",
     title: "Rock N` Roll Blunts",
     description: `Company about Cannabis Sales. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [AstroIcon, TailWindCssIcon],
+    tools: ["astro", "tailwindcss"],
   },
   {
     status: "production",
     linkPreview: "https://www.aortegaconstruction.com",
     linkCode: undefined,
-    srcImg: "project-a-ortega-construction.png",
+    srcImg: "projects/project-a-ortega-construction.png",
     title: "A Ortega Construction",
     description: `Construction Company Landing Page. Through the site, their 
         clients were able to learn about their services and get in touch with the company`,
-    tools: [AstroIcon, TailWindCssIcon],
+    tools: ["astro", "tailwindcss"],
   },
 ];
 export default data;
